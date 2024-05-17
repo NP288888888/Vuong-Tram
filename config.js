ecd1b3e7d9a76dab0ee1d6f453f2bc2132d450fe
NP288888888 <162107583+NP288888888@users.noreply.github.com>
2024-05-17 09:23:49 +0700

    Add files via upload
    ok

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Vuong Tram';
+const giftUrl = 'https://350new.com';
+const eventName = 'Chúc Mừng 01-06';
+const titleCard = 'Chị Yêu Quý';
+const contentCard = 'Chúc chị 01/06 tràn ngập niềm vui và những nụ cười.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Wecomel https://www.facebook.com/vn.NguyenAnhPhuong
+// Các bạn muốn học lập trình thì tham gia nhóm tự học lập trình nhé: https://www.facebook.com/vn.NguyenAnhPhuong
+const giftImage = '8888-.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
